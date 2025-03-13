083233fe12bec110051009c2c5fa27547b5da0ab
Timilehin Daramola <timi.homezone111@gmail.com>
2025-03-12 22:46:22 -0400

    cse341

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
-const app = express()
+
+const mongodb = require('/data/database');
+const app = express();
 const port = 3000
 
 app.use('/', require("./routes"))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening on port ${port}`)
+          })
+    }
 })
+
+
