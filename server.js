25333bddf681c6a73bcd44f362878430b3be1edb
Timilehin Daramola <timi.homezone111@gmail.com>
2025-03-13 01:00:45 -0400

    cse341

--- server.js
+++ server.js
@@ -7,13 +7,38 @@ const port = process.env.PORT || 3000;
 app.use('/', require("./routes"));
 
 
-mongodb.initDb((err) => {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        app.listen(port, () => {
-            console.log(`Database is listening on port ${port}`);
-          });
-    }
-});
+// mongodb.initDb((err) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         app.listen(port, () => {
+//             console.log(`Database is listening on port ${port}`);
+//           });
+//     }
+// });
+
+
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://timilehin:timi12345@cluster0.zpbf3.mongodb.net/";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
